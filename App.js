2bd3af41987252cf9097365cb668d74a8399fc95
jenish4809 <jenishasodariya4809@gmail.com>
2024-04-04 12:46:08 +0530

    Create Project and set route from index to app.js

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import AppNavigator from "./src/navigation";
+import { styles } from "./src/themes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    nunitoBlack: require("./src/assets/fonts/Nunito-Black.ttf"),
+    nunitoBold: require("./src/assets/fonts/Nunito-Bold.ttf"),
+    nunitoBoldItalic: require("./src/assets/fonts/Nunito-BoldItalic.ttf"),
+    nunitoExtraBold: require("./src/assets/fonts/Nunito-ExtraBold.ttf"),
+    nunitoItalic: require("./src/assets/fonts/Nunito-Italic.ttf"),
+    nunitoLight: require("./src/assets/fonts/Nunito-Light.ttf"),
+    nunitoMedium: require("./src/assets/fonts/Nunito-Medium.ttf"),
+    nunitoRegular: require("./src/assets/fonts/Nunito-Regular.ttf"),
+    nunitoSemiBold: require("./src/assets/fonts/Nunito-SemiBold.ttf"),
+    coiniRegular: require("./src/assets/fonts/Coiny-Regular.ttf"),
+    workmedium: require("./src/assets/fonts/WorkSans-Medium.ttf"),
+  });
+  if (!fontsLoaded) {
+    return undefined;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.flex}>
+      <ExpoStatusBar />
+      <AppNavigator />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
