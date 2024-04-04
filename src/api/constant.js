c11b4bab4595d56aa47b088c6d7ad8f3798867da
jenish4809 <jenishasodariya4809@gmail.com>
2024-04-04 12:48:36 +0530

    Make Constant.js for OnBoarding data

---
+++ src/api/constant.js
@@ -0,0 +1,26 @@
+import images from "../assets/images";
+
+export const OnBoardingData = [
+  {
+    id: 1,
+    colorLogo: images.logotextcolor,
+    onBoardingImg: images.onBoarding1,
+    title: "Eat Healthy",
+    description:
+      "Maintaining good health should be the primary focus of everyone.",
+  },
+  {
+    id: 2,
+    colorLogo: images.logotextcolor,
+    onBoardingImg: images.onBoarding2,
+    title: "Healthy Recipes",
+    description: "Browse thousands of healthy recipes from all over the world.",
+  },
+  {
+    id: 3,
+    colorLogo: images.logotextcolor,
+    onBoardingImg: images.onBoarding3,
+    title: "Track Your Health",
+    description: "With amazing inbuilt tools you can track your progress.",
+  },
+];
